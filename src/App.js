2fe3dd51406ced97fa3c18584d05956e641fe28c
Evgeny Musicantov <evgeny.musicantov@pagaya.com>
2023-10-06 01:21:14 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Employee from './components/Emporyee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev')
+
   return (
     <div className="App">
-      <header className="App-header">
+      <input type='text' onChange={(e) =>
+      {
+        // console.log(e.target.value);
+        setRole(e.target.value);
+      } }/>
         <Employee name = 'Caleb' role ='junkyard'/>
-        <Employee name = 'Jena'/>
+        <Employee name = 'Jena' role={role}/>
         <Employee name = 'Alisa' role='Boss'/>
-
-      </header>
     </div>
   );
 }
