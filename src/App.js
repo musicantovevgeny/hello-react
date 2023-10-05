65f859e67bc08e2d34599ebe2b79ca4e7b9af56c
Evgeny Musicantov <evgeny.musicantov@pagaya.com>
2023-10-06 00:50:29 +0300

    added employee props and roles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Employee from './components/Emporyee';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        HELLO1 and stuff112
+        <Employee name = 'Caleb' role ='junkyard'/>
+        <Employee name = 'Jena'/>
+        <Employee name = 'Alisa' role='Boss'/>
+
       </header>
     </div>
   );
