d3b281ef05afaaf98954ed710b1eb8539fe9d9ad
Evgeny Musicantov <evgeny.musicantov@pagaya.com>
2023-10-06 14:45:21 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,19 +2,52 @@ import './App.css';
 import Employee from './components/Emporyee';
 import { useState } from 'react';
 
+
+
 function App() {
+
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name: 'Caleb', 
+        role:'Developer', 
+        img:'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg'
+      },
+      {
+        name: 'Jena', 
+        role:'Data Scientist', 
+        img: 'https://images.pexels.com/photos/2743754/pexels-photo-2743754.jpeg'
+      },
+      {
+        name: 'Alisa', 
+        role:'Boss', 
+        img: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg'
+      },
+      {
+        name: 'Caleb', 
+        role:'Developer', 
+        img:'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg'
+      },
+      {
+        name: 'Jena', 
+        role:'Data Scientist', 
+        img: 'https://images.pexels.com/photos/2743754/pexels-photo-2743754.jpeg'
+      },
+      {
+        name: 'Alisa', 
+        role:'Boss', 
+        img: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg'
+      }
+    ]
+  )
+  console.log(employees);
 
   return (
     <div className="App">
       <div className="flex flex-wrap">
-      <Employee name = 'Caleb' role ='junkyard' image='https://images.unsplash.com/photo-1534614971-6be99a7a3ffd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'/>
-        <Employee name = 'Jena' image='https://images.unsplash.com/photo-1512663150964-d8f43c899f76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1984&q=80' />
-        <Employee name = 'Alisa' role='Boss' image = 'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80'/>
-        <Employee name = 'Caleb' role ='junkyard'/>
-        <Employee name = 'Jena'/>
-        <Employee name = 'Alisa' role='Boss'/>
-        <Employee name = 'Caleb' role ='junkyard'/>
-        <Employee name = 'Jena'/>
+        {employees.map((employee)=> {
+          return (<Employee name={employee.name} role={employee.role} img={employee.img} />)
+        })}
       </div>
     </div>
   );
--- src/components/Emporyee.js
+++ src/components/Emporyee.js
@@ -1,7 +1,11 @@
+
+import EditEmployee from "./EditEmployee";
+
+
 function Employee(props) {
     return (
         <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img className="objrct-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.image} alt="Missing image" />
+            <img className="objrct-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="Missing image" />
             <div className="text-center space-y-2 sm:text-left">
                  <div className="space-y-0.5">
                     <p className="text-lg text-black font-semibold">
@@ -11,7 +15,7 @@ function Employee(props) {
                     {props.role}
                     </p>
                 </div>
-           <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+            <EditEmployee />
         </div>
       </div>
     )
