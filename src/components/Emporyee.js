8a6f2a6f2ba21ecb98f545869ebe91aed243ce72
Evgeny Musicantov <evgeny.musicantov@pagaya.com>
2023-10-06 12:19:37 +0300

    initial tailwind commit

--- src/components/Emporyee.js
+++ src/components/Emporyee.js
@@ -1,7 +1,7 @@
 function Employee(props) {
     return (
     <>
-        <h3>Employee {props.name}</h3>
+        <h3  className="text-3xl font-bold underline">Employee {props.name}</h3>
         <p>{props.role ? props.role : "No role"}</p>
     </>
     )
