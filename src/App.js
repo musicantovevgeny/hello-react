95b931620e15f4c03584546f935cbe1f086a7b62
Evgeny Musicantov <evgeny.musicantov@pagaya.com>
2023-10-06 12:57:30 +0300

    removed inputbox added employees

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Employee from './components/Emporyee';
 import { useState } from 'react';
 
 function App() {
-  const [role, setRole] = useState('dev')
 
   return (
     <div className="App">
-      <input type='text' onChange={(e) =>
-      {
-        // console.log(e.target.value);
-        setRole(e.target.value);
-      } }/>
+
         <Employee name = 'Caleb' role ='junkyard'/>
-        <Employee name = 'Jena' role={role}/>
+        <Employee name = 'Jena' />
+        <Employee name = 'Alisa' role='Boss'/>
+        <Employee name = 'Caleb' role ='junkyard'/>
+        <Employee name = 'Jena'/>
+        <Employee name = 'Alisa' role='Boss'/>
+        <Employee name = 'Caleb' role ='junkyard'/>
+        <Employee name = 'Jena'/>
         <Employee name = 'Alisa' role='Boss'/>
     </div>
   );
