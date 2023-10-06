ceaae6a0e3e0e74e21b8cd63fff24d2f84f5f755
Evgeny Musicantov <evgeny.musicantov@pagaya.com>
2023-10-06 13:11:30 +0300

    removed stuff from index.css added margin to employeee component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
         <Employee name = 'Alisa' role='Boss'/>
         <Employee name = 'Caleb' role ='junkyard'/>
         <Employee name = 'Jena'/>
-        <Employee name = 'Alisa' role='Boss'/>
       </div>
     </div>
   );
