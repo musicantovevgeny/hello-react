a64fa1395d04978d06ee4dd2d80c7d0d45952b9b
Evgeny Musicantov <evgeny.musicantov@pagaya.com>
2023-10-06 13:57:53 +0300

    rounded images

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-wrap">
-      <Employee name = 'Caleb' role ='junkyard'/>
-        <Employee name = 'Jena' />
-        <Employee name = 'Alisa' role='Boss'/>
+      <Employee name = 'Caleb' role ='junkyard' image='https://images.unsplash.com/photo-1534614971-6be99a7a3ffd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'/>
+        <Employee name = 'Jena' image='https://images.unsplash.com/photo-1512663150964-d8f43c899f76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1984&q=80' />
+        <Employee name = 'Alisa' role='Boss' image = 'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80'/>
         <Employee name = 'Caleb' role ='junkyard'/>
         <Employee name = 'Jena'/>
         <Employee name = 'Alisa' role='Boss'/>
--- src/components/Emporyee.js
+++ src/components/Emporyee.js
@@ -1,7 +1,7 @@
 function Employee(props) {
     return (
-        <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/erin-lindford.jpg" alt="Woman's Face" />
+        <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <img className="objrct-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.image} alt="Missing image" />
             <div className="text-center space-y-2 sm:text-left">
                  <div className="space-y-0.5">
                     <p className="text-lg text-black font-semibold">
